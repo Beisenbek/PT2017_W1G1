c3067f525aee3b1d173d1a16054c34c5bfd1fb42
Beisenbek <beysenbek@gmail.com>
2017-01-16 10:42:30 +0600

    Added 3rd example

---
+++ Example3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example3
+{
+    class Student
+    {
+        public int age;
+        public string name;
+        public void GetInfo()
+        {
+            Console.WriteLine(age + " " + name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.age = 20;
+            s.name = "Sapar";
+            s.GetInfo();
+        }
+    }
+}
