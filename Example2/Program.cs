8acaa9ca9c146216fa5723934a84e55c8cd026f8
Beisenbek <beysenbek@gmail.com>
2017-01-16 10:22:49 +0600

    Added two basic examples

---
+++ Example2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                int a = int.Parse(args[0]);
+                int b = int.Parse(args[1]);
+                Console.WriteLine(a + b);
+            }
+            else
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int c = a + b;
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
